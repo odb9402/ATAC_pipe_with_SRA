b65955f5485fbc204815ce168d356b1b18de431a
odb9402 <dhehdqls@gmail.com>
2019-08-07 01:09:58 +0900

    add download scripts

---
+++ bulk_ATAC_download.sh
@@ -0,0 +1,27 @@
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356168
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356167
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356166
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356165
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356164
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356163
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356162
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356161
+wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356160
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356159
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356158
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356157
+#wget https://sra-download.ncbi.nlm.nih.gov/traces/sra46/SRR/005230/SRR5356156
+
+#sam-dump SRR5356168| samtools view -bS - > SRR5356168.bam
+#sam-dump SRR5356167| samtools view -bS - > SRR5356167.bam
+#sam-dump SRR5356166| samtools view -bS - > SRR5356166.bam
+#sam-dump SRR5356165| samtools view -bS - > SRR5356165.bam
+#sam-dump SRR5356164| samtools view -bS - > SRR5356164.bam
+#sam-dump SRR5356163| samtools view -bS - > SRR5356163.bam
+#sam-dump SRR5356162| samtools view -bS - > SRR5356162.bam
+#sam-dump SRR5356161| samtools view -bS - > SRR5356161.bam
+#sam-dump SRR5356160| samtools view -bS - > SRR5356160.bam
+#sam-dump SRR5356159| samtools view -bS - > SRR5356159.bam
+#sam-dump SRR5356158| samtools view -bS - > SRR5356158.bam
+#sam-dump SRR5356157| samtools view -bS - > SRR5356157.bam
+#sam-dump SRR5356156| samtools view -bS - > SRR5356156.bam
--- pipe_temp.sh
+++ pipe_temp.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 
 
-#### EXTRACT FASTQ
-echo "FASTQ-dump from SRA data $1 . . .\n"
+#### EXTRACT FASTQ FROM SRA FILE
+echo -e "FASTQ-dump from SRA data $1 . . .\n"
 prefix=$1
-fastq_file="$1.fastq.gz\n"
+fastq_file="$1.fastq.gz"
 
 #fastq-dump -Z --gzip $1 > $fastq_file
-echo "$fastq_file was generated.\n"
+echo -e "$fastq_file was generated.\n"
+
+
 
 ### DETECT AND REMOVE ADAPTERS
-echo "Detecting the adapter sequence . . .\n"
+echo -e "Detecting the adapter sequence . . .\n"
 log="$fastq_file.adapter.txt"
 adapter_err_rate=0.2
 #python detect_adapter.py $fastq_file > $log
-echo "Parsing The adapter sequence . . .\n"
+echo -e "Parsing The adapter sequence . . .\n"
 
 trimming_command="tr -d  \040\011\012\015"
 adapter_from="$(tail -1 $log | $trimming_command)"
 
-echo "Adapter from :: $adapter_from\n"
+echo -e "Adapter from :: $adapter_from \n"
 #adapter_seq=$(awk -v adapt="$adapter_from" '($1==adapt&&$2!=NULL){print $3}' $log)
-echo "Adapter sequence: $adapter_seq\n"
-
+echo -e "Adapter sequence: $adapter_seq \n"
 trimmed_fastq="$prefix.trim.fastq.gz"
 #cutadapt -m 5 -e 0.2 -a $adapter_seq $fastq_file | gzip -c > $trimmed_fastq
 
+#rm $fastq_file
+
+
+
 ## ALIGNMENT WITH BOWTIE2 (FOR PAIRED END)
 multimapping=4
+core_num=28
 bwt2_idx="hg19"
 bam="$prefix.bam"
 log_align="$prefix.align.log"
 
-bowtie2 -k $multimapping --local -x $bwt2_idx --threads 6 -U <(zcat -f $fastq_file) 2> $log_align | samtools view -Su /dev/stdin | samtools sort -o $prefix.bam
+echo -e "Sequence alignment with bowtie2 . . . \n"
+
+bowtie2 -k $multimapping --local -x $bwt2_idx --threads $core_num -U <(zcat -f $trimmed_fastq) 2> $log_align | samtools view -Su /dev/stdin | samtools sort -o $prefix.bam
