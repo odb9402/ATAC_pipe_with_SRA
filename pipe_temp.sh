0603860dc68cdea79775c9d45722ca135f020b33
odb9402 <dhehdqls@gmail.com>
2019-09-04 00:15:27 +0900

    change pipe_temp.sh

--- pipe_temp.sh
+++ pipe_temp.sh
@@ -2,28 +2,28 @@
 
 
 #### EXTRACT FASTQ FROM SRA FILE
-echo -e "FASTQ-dump from SRA data $1 . . .\n"
+echo -e "$(date) FASTQ-dump from SRA data $1 . . .\n"
 prefix=$1
 fastq_file="$1.fastq.gz"
 
 fastq-dump -Z --gzip $1 > $fastq_file
-echo -e "$fastq_file was generated.\n"
+echo -e "$(date)$fastq_file was generated.\n"
 
 
 
 ### DETECT AND REMOVE ADAPTERS
-echo -e "Detecting the adapter sequence . . .\n"
+echo -e "$(date)Detecting the adapter sequence . . .\n"
 log="$fastq_file.adapter.txt"
 adapter_err_rate=0.2
 python detect_adapter.py $fastq_file > $log
-echo -e "Parsing The adapter sequence . . .\n"
+echo -e "$(date)Parsing The adapter sequence . . .\n"
 
 trimming_command="tr -d  \040\011\012\015"
 adapter_from="$(tail -1 $log | $trimming_command)"
 
-echo -e "Adapter from :: $adapter_from \n"
+echo -e "$(date)Adapter from :: $adapter_from \n"
 adapter_seq=$(awk -v adapt="$adapter_from" '($1==adapt&&$2!=NULL){print $3}' $log)
-echo -e "Adapter sequence: $adapter_seq \n"
+echo -e "$(date)Adapter sequence: $adapter_seq \n"
 trimmed_fastq="$prefix.trim.fastq.gz"
 cutadapt -m 5 -e 0.2 -a $adapter_seq $fastq_file | gzip -c > $trimmed_fastq
 
@@ -38,7 +38,7 @@ bwt2_idx="hg19"
 bam="$prefix.bam"
 log_align="$prefix.align.log"
 
-echo -e "Sequence alignment with bowtie2 . . . \n"
+echo -e "$(date)Sequence alignment with bowtie2 . . . \n"
 
 bowtie2 -k $multimapping --local -x $bwt2_idx --threads $core_num -U <(zcat -f $trimmed_fastq) 2> $log_align | samtools view -Su /dev/stdin | samtools sort -o $prefix.bam
 
@@ -53,7 +53,7 @@ filt_bam_prefix="$prefix.filt.srt"
 filt_bam="$filt_bam_prefix.bam"
 
 
-echo -e "REMOVE UNMAPPED READS . . . \n"
+echo -e "$(date)REMOVE UNMAPPED READS . . . \n"
 samtools sort -n $bam -T $qname_sort_prefix -o $qname_sort_bam
 samtools view -h $qname_sort_bam | python2 assign_multimappers.py -k $multimapping | samtools view -bS > $qname_sort_bam_filt
 
@@ -63,6 +63,7 @@ rm -f $qname_sort_bam $qname_sort_bam_filt
 rm $bam
 rm $trimmed_fastq
 
+echo -e "$(date)UNMAPPED READS REMOVED. . . \n"
 
 ### MARK DUMPLICATES
 
@@ -72,6 +73,8 @@ dup_file_qc="$filt_bam_prefix.dup.qc"
 final_bam_prefix="$prefix.final"
 final_bam="$final_bam_prefix.bam"
 
+echo -e "$(date)MARK DUPLICATES . . . . \n"
+
 bamtools sort -in $filt_bam > "$filt_bam.tmpsrt"
 
 rm $filt_bam
@@ -80,5 +83,9 @@ java -jar picard.jar MarkDuplicates I="$filt_bam.tmpsrt" O=$tmp_filt_bam_file M=
 
 rm "$filt_bam.tmpsrt"
 
+echo -e "$(date)FILTERING DUPLICATES . . . . \n"
+
 samtools view -F 1804 -b $tmp_filt_bam_file > $final_bam
+
+echo -e "$(date)FILTERING DUPLICATES  . . . . \n"
 rm $tmp_filt_bam_file
